07773b5c81b19c296708f16c9ce3232482bedbe7
NormanAb <normanasabi@gmail.com>
2024-11-05 10:42:17 +0200

    Add files via upload

---
+++ books/src/main/java/com/example/books/controllers/AuthorController.java
@@ -0,0 +1,10 @@
+package com.example.books.controllers;
+
+import com.example.books.utils.SceneLoader;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+public class AuthorController {
+}
